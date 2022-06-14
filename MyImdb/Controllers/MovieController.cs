dea282a3903b3e6190bc51507f015ab8f23ea801
Gabrielle Guimarães de Oliveira <gabrielle1guim@gmail.com>
2022-06-14 07:30:21 -0300

    feat: add form validation

---
+++ MyImdb/Controllers/MovieController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyImdb.ViewModels;
+
+namespace MyImdb.Controllers;
+
+public class MovieController : Controller {
+	// GET
+	public IActionResult Index() {
+		return RedirectToPage("/");
+	}
+
+	// GET
+	public IActionResult Create() {
+		return View();
+	}
+
+	// POST
+	[HttpPost]
+	public IActionResult Create(CreateMovieViewModel model) {
+		return ModelState.IsValid ? View(model) : View();
+	}
+}
