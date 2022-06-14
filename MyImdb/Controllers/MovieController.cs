9e8e7181b02e75c9bcb4f1bc66d8aaa627ef0968
Gabrielle Guimarães de Oliveira <gabrielle1guim@gmail.com>
2022-06-14 08:02:39 -0300

    feat: redirect to movie list when create movie successfully

--- MyImdb/Controllers/MovieController.cs
+++ MyImdb/Controllers/MovieController.cs
@@ -6,10 +6,12 @@ namespace MyImdb.Controllers;
 
 public class MovieController : Controller {
 	// GET
-	public IActionResult Index() {
+	public IActionResult Index(string? message = null) {
 		var movies = Movie
 			.SelectAll()
 			.ConvertAll(movie => new MovieListViewModel { Rank = movie.Rank, Title = movie.Title, Year = movie.Year, });
+
+		ViewBag.Message = message;
 
 		return View(movies);
 	}
@@ -22,6 +24,10 @@ public class MovieController : Controller {
 	// POST
 	[HttpPost]
 	public IActionResult Create(CreateMovieViewModel model) {
-		return ModelState.IsValid ? View(model) : View();
+		// Aqui usa-se nameof(Index) porque a função RedirectToAction, requer um parametro string, e usar apenas Index,
+		// resultaria numa referência de função.
+		return ModelState.IsValid
+			? RedirectToAction(nameof(Index), new { message = "Movie created successfully!" })
+			: View(model);
 	}
 }
