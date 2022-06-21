d42ca41429fa022540c86e06c377087bc3ab4146
Gabrielle Guimarães de Oliveira <gabrielle1guim@gmail.com>
2022-06-21 13:36:20 -0300

    feat: set up migrations

--- MyImdb/Entities/AppDbContext.cs
+++ MyImdb/Entities/AppDbContext.cs
@@ -3,8 +3,8 @@
 namespace MyImdb.Entities;
 
 public class AppDbContext : DbContext {
-	public DbSet<Movie> Movies { get; set; }
-
 	public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) {
 	}
+
+	public DbSet<Movie> Movies { get; set; }
 }
