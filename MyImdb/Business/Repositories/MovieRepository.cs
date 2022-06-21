bb0a9065b90013c9051caeb11e538ea2293cff9d
Gabrielle Guimarães de Oliveira <gabrielle1guim@gmail.com>
2022-06-21 14:44:44 -0300

    feat: add MovieRepository

---
+++ MyImdb/Business/Repositories/MovieRepository.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyImdb.Entities;
+
+namespace MyImdb.Business.Repositories; 
+
+public class MovieRepository {
+	private readonly AppDbContext dbContext;
+
+	public MovieRepository(AppDbContext dbContext) {
+		this.dbContext = dbContext;
+	}
+	
+	public async Task<List<Movie>> SelectAllAsync() {
+		return await dbContext.Movies.ToListAsync();
+	}
+}
