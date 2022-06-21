dcc00e6341309add284e1878c902ee9aa39b283d
Gabrielle Guimarães de Oliveira <gabrielle1guim@gmail.com>
2022-06-21 14:59:10 -0300

    feat(Api): add ApiException

---
+++ Api/ApiException.cs
@@ -0,0 +1,13 @@
+﻿namespace Api;
+
+public class ApiException {
+	public ErrorModel Error { get; }
+
+	public ApiException(ErrorModel error) {
+		Error = error;
+	}
+
+	public static ExceptionBuilder Builder() {
+		return new ExceptionBuilder();
+	}
+}
---
+++ Api/ExceptionBuilder.cs
@@ -0,0 +1,26 @@
+﻿namespace Api;
+
+public class ExceptionBuilder {
+	public ApiException Api(ErrorCode code, object? details = null) {
+		var message = code switch {
+			ErrorCode.Unknown => "An unknown error occurred",
+			_ => ""
+		};
+
+		return new ApiException(new ErrorModel {
+			Code = code,
+			Message = message,
+			Details = details == null ? new Dictionary<string, string>() : getDetailsDictionary(details),
+		});
+	}
+
+	private static Dictionary<string, string> getDetailsDictionary(object details) {
+		var dictionary = new Dictionary<string, string>();
+
+		foreach (var property in details.GetType().GetProperties()) {
+			dictionary[property.Name] = property.GetValue(details, null)?.ToString() ?? "null";
+		}
+
+		return dictionary;
+	}
+}
