80c243814ced997d5de1364e05069e4e5f2856bf
Gabrielle Guimarães de Oliveira <gabrielle1guim@gmail.com>
2022-06-21 14:46:23 -0300

    refactor: refactor SelectAllAsync to SelectTopNAsync

--- MyImdb/Business/Repositories/MovieRepository.cs
+++ MyImdb/Business/Repositories/MovieRepository.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using MyImdb.Entities;
 
-namespace MyImdb.Business.Repositories; 
+namespace MyImdb.Business.Repositories;
 
 public class MovieRepository {
 	private readonly AppDbContext dbContext;
@@ -9,8 +9,12 @@ public class MovieRepository {
 	public MovieRepository(AppDbContext dbContext) {
 		this.dbContext = dbContext;
 	}
-	
-	public async Task<List<Movie>> SelectAllAsync() {
-		return await dbContext.Movies.ToListAsync();
+
+	public async Task<List<Movie>> SelectTopNAsync(int n = 20) {
+		return await dbContext.Movies
+			.OrderBy(movie => movie.Title)
+			.AsQueryable()
+			.Take(n)
+			.ToListAsync();
 	}
 }
