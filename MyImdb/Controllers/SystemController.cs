389d3de9c282d16a541510b0d78e68aad6095ed8
Gabrielle Guimarães de Oliveira <gabrielle1guim@gmail.com>
2022-06-22 06:55:57 -0300

    feat(MyImdb): add system info route

---
+++ MyImdb/Controllers/SystemController.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyImdb.Controllers;
+
+[ApiController]
+public class SystemController : Controller {
+	[HttpGet("/api/system")]
+	public object GetSystemInfo() {
+		var assembly = Assembly.GetEntryAssembly()!;
+		var name = assembly.GetName();
+
+		var version = name.Version!;
+
+		return new { version = $"{version.Major}.{version.Minor}.{version.Build}" };
+	}
+}
